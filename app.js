4ea8ed3f995ddbec29a113c71a2232e29b4e2363
1190100 <1190100@isep.ipp.pt>
2020-11-19 16:43:01 +0000

    wrong files commited

--- app.js
+++ app.js
@@ -1,107 +1,52 @@
-document.getElementById()
-document.getElementsByClassName()
-document.getElementsByTagName()
-document.querySelector()
-document.querySelectorAll()
+async function getUserName() {
+
+    document.getElementById('status').innerHTML = 'LOADING';
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+
+    if (response.status !== 200) {
+        document.getElementById('status').innerHTML = 'ERROR contacting api';
+    } else {
+        document.getElementById('status').innerHTML = '';
+    }
+
+    const data = await response.json()
 
 
-// const updateUI = (data) => {
-//     document.getElementById("score-tag").innerHTML = `Score tag retrieved: ${data.score_tag}`;
-//     document.getElementById("agreement").innerHTML = `Agreement retrieved: ${data.agreement}`;
-//     document.getElementById("subjectivity").innerHTML = `Subjectivity retrieved: ${data.subjectivity}`;
-//     document.getElementById("confidence").innerHTML = `Confidence retrieved: ${data.confidence}`;
-//     document.getElementById("irony").innerHTML = `Irony retrieved: ${data.irony}`;
+    let cardsHtml = "";
+    data.forEach((card) => {
+        cardsHtml += `
+       <li class="card">
+        <div>
+             <span>User:</span>
+             <span>${card.name}</span>
+        </div>
 
+        <div>
+             <span>Username:</span>
+             <span>${card.username}</span>
+        </div>
 
+        <div>
+             <span>Email:</span>
+             <span>${card.email}</span>
+        </div>
 
-// SERVER SIDE!!!
-// app.get('/', function(req, res) {
+        <div>
+             <span>Phone:</span>
+             <span>${card.phone}</span>
+        </div>
 
-//     res.sendFile('dist/index.html')
-// })
+        <div>
+             <span>Address:</span>
+             <span>${card.address.street} , ${card.address.suite} , ${card.address.city}  ${card.address.zipcode}</span>
+        </div>
+    </li>
+    `
+    })
 
-// app.get('/test', function(req, res) {
-//     res.json(mockAPIResponse)
-// })
+    document.getElementById('card-container').innerHTML = cardsHtml;
+    document.getElementById('status').innerHTML = 'DONE';
 
-//designates what port the app will listen to for incoming requests
-// app.listen(8080, function() {
-//     console.log('Example app listening on port 8080!')
-// })
+}
 
-// app.post('/evaluate-articles', async(req, res) => {
-
-//     let params = {
-//         key: apiKey,
-//         lang: 'en',
-//         url: req.body.formUrl,
-//         txt: req.body.formText
-//     };
-
-//     const result = await axios({
-//         url: 'http://api.meaningcloud.com/sentiment-2.1',
-//         method: "post",
-//         params: params,
-//     })
-
-//     const { data } = result;
-//     const { code } = data.status;
-//     const { score_tag } = data;
-//     const { agreement } = data;
-//     const { subjectivity } = data;
-//     const { confidence } = data;
-//     const { irony } = data;
-
-//     // storing the api response
-//     sentiment = {
-//         score_tag,
-//         agreement,
-//         subjectivity,
-//         confidence,
-//         irony,
-//     };
-
-//     res.send(sentiment);
-// })
-
-
-// ANOTHER SERVER SIDE:
-
-// const { response } = require('express');
-// const express = require('express');
-
-// express app
-const app = express();
-
-// register vue engine
-
-// app.set('view engine', 'ejs');
-
-// if i needed view engine to check for the html files :
-// app.set('views', 'myvies');
-// listen for requests
-
-app.listen(3000, );
-
-app.get('/', (req, res) => {
-    //res.send('<p> HomePage </p>');
-    res.sendFile('./views/index.html', { root: __dirname });
-})
-
-
-app.get('/about', (req, res) => {
-    //res.send('<p> AboutPage </p>');
-    res.sendFile('./views/about.html', { root: __dirname });
-})
-
-
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-})
-
-// 404 app.use middleware
-
-app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
-})
+getUserName();
